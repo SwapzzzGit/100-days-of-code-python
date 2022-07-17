fcc1e576743686e2eeac9acf9d54e88eacc01042
Swapnil Mundaware <93301234+SwapzzzGit@users.noreply.github.com>
2022-07-17 13:36:56 +0530

    Add files via upload

---
+++ day_3/day-3-3-exercise/main_1.py
@@ -0,0 +1,14 @@
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+BMI = round(weight/height ** 2)
+print(BMI)
+if BMI < 18.511:
+  print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI >= 18.511 and BMI < 25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI >= 25 and BMI < 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI >= 30 and BMI < 35 :
+  print(f"Your BMI is {BMI}, you are obese.")
+else :
+  print(f"Your BMI is {BMI},you are clinically obese.")
